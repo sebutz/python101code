c47df67f7f0c061fecea58deb8239797a47fd577
sebastian gherghesanu <sebastian.gherghesanu@tribalworldwide.co.uk>
2019-01-08 10:36:29 +0200

    chapter2 : run & learn

--- Chapter 2 - Strings/creating_strings.py
+++ Chapter 2 - Strings/creating_strings.py
@@ -18,8 +18,22 @@ my_string = "I'm a Python programmer!"
 otherString = 'The word "python" usually refers to a snake'
 tripleString = """Here's another way to embed "quotes" in a string"""
 
-# cast an integer to a string using Python's str() class
+# checking the type
 my_number = 123
+print("type of my_number:", type(my_number))
+
+# checking the id of teh object
+print("id of my_number:", id(my_number))
+
+# re-assign
+my_number = 234
+print("id after re-assignment of my_number:", id(my_number)) # id changed
+
+
+my_number2 = 22.0
+print("type of my_number2:", type(my_number2)) # float
+
+# cast an integer to a string using Python's str() class
 my_string = str(my_number)
 
 print(my_number)
